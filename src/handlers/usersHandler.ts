68e72993df81afea62f669cfa67b48010fdf7a95
Alfredo Torres <atorres9902@gmail.com>
2024-11-10 13:56:24 -0400

    agregando joi

--- src/handlers/usersHandler.ts
+++ src/handlers/usersHandler.ts
@@ -2,225 +2,237 @@ import bcrypt from "bcrypt";
 import colors from "colors";
 import Users from "../models/Users.model.js";
 import {
-	ReqCreateUser,
-	ReqGetOneUser,
-	ReqGetUsers,
-	ReqUpdateUser,
+  ReqCreateUser,
+  ReqGetOneUser,
+  ReqGetUsers,
+  ReqUpdateUser,
 } from "../types/users";
 import { RequestAPI, ResponseAPI } from "../types/express";
 import Roles from "../models/Roles.model.js";
 import { Op } from "sequelize";
+import { validateUser } from "../schemas/usersJoi.js";
 
 const getUsers = async (req: ReqGetUsers, res: ResponseAPI) => {
-	try {
-		let query: string | string[] = req.query.rol;
+  try {
+    let query: string | string[] = req.query.rol;
 
-		if (query == "all") {
-			query = ["admin", "user"];
-		} else if (query != "encargado") {
-			res.status(404).json({
-				status: false,
-				message: "El rol no existe",
-			});
-			return;
-		}
+    if (query == "all") {
+      query = ["admin", "user"];
+    } else if (query != "encargado") {
+      res.status(404).json({
+        status: false,
+        message: "El rol no existe",
+      });
+      return;
+    }
 
-		let users = await Users.findAll({
-			attributes: { exclude: ["password", "createdAt", "updatedAt"] },
-			include: [
-				{
-					model: Roles,
-					where: {
-						rol: query,
-					},
-					attributes: { exclude: ["createdAt", "updatedAt"] },
-				},
-			],
-		});
+    let users = await Users.findAll({
+      attributes: { exclude: ["password", "createdAt", "updatedAt"] },
+      include: [
+        {
+          model: Roles,
+          where: {
+            rol: query,
+          },
+          attributes: { exclude: ["createdAt", "updatedAt"] },
+        },
+      ],
+    });
 
-		res.status(200).json({
-			status: true,
-			message: "Consulta exitosa",
-			data: users,
-		});
-		return;
-	} catch (error) {
-		console.log(
-			colors.bgRed.white(
-				`Ocurrio un error en la creacion de usuarios. ${error}`
-			)
-		);
-		res.status(500).json({
-			status: false,
-			message: "Ocurrio un error en el servidor",
-		});
-		return;
-	}
+    res.status(200).json({
+      status: true,
+      message: "Consulta exitosa",
+      data: users,
+    });
+    return;
+  } catch (error) {
+    console.log(
+      colors.bgRed.white(
+        `Ocurrio un error en la creacion de usuarios. ${error}`
+      )
+    );
+    res.status(500).json({
+      status: false,
+      message: "Ocurrio un error en el servidor",
+    });
+    return;
+  }
 };
 const getOneUser = async (req: ReqGetOneUser, res: ResponseAPI) => {
-	try {
-		let { id } = req.params;
+  try {
+    let { id } = req.params;
 
-		let user = await Users.findByPk(id, {
-			include: {
-				model: Roles,
-			},
-			attributes: { exclude: ["password", "createdAt", "updatedAt"] },
-		});
+    let user = await Users.findByPk(id, {
+      include: {
+        model: Roles,
+      },
+      attributes: { exclude: ["password", "createdAt", "updatedAt"] },
+    });
 
-		if (!user) {
-			res.status(400).json({
-				status: false,
-				message: "El usuario no existe",
-			});
-			return;
-		}
+    if (!user) {
+      res.status(400).json({
+        status: false,
+        message: "El usuario no existe",
+      });
+      return;
+    }
 
-		res.status(200).json({
-			status: true,
-			message: "Datos actualizados correctamente",
-			data: user,
-		});
-		return;
-	} catch (error) {
-		console.log(
-			colors.bgRed.white(
-				`Ocurrio un error en la actualizacion de usuarios. ${error}`
-			)
-		);
-		res.status(500).json({
-			status: false,
-			message: "Ocurrio un error en el servidor",
-		});
-	}
+    res.status(200).json({
+      status: true,
+      message: "Datos actualizados correctamente",
+      data: user,
+    });
+    return;
+  } catch (error) {
+    console.log(
+      colors.bgRed.white(
+        `Ocurrio un error en la actualizacion de usuarios. ${error}`
+      )
+    );
+    res.status(500).json({
+      status: false,
+      message: "Ocurrio un error en el servidor",
+    });
+  }
 };
 
 const createUser = async (req: ReqCreateUser, res: ResponseAPI) => {
-	try {
-		// const salt = await bcrypt.genSalt(10);
+  try {
+    // const salt = await bcrypt.genSalt(10);
 
-		let role = await Roles.findByPk(req.body.roleId);
+    let { values, error } = validateUser(req.body);
 
-		if (!role) {
-			res.status(404).json({
-				status: false,
-				message: "El rol no existe",
-			});
-			return;
-		}
-		let usr = {
-			...req.body,
-		};
+    if (error) {
+      res.status(400).json({
+        status: false,
+        message: "Mal formato de informacion",
+        data: error.details.map((item) => item.message),
+      });
+      return;
+    }
 
-		let user = await Users.findOne({
-			where: {
-				[Op.or]: [{ email: usr.email }, { username: usr.username }],
-			},
-		});
+    let role = await Roles.findByPk(req.body.roleId);
 
-		if (user) {
-			if (user.dataValues.username === usr.username) {
-				res.status(400).json({
-					status: false,
-					message: "El nombre de usuario ya existe",
-				});
-				return;
-			}
+    if (!role) {
+      res.status(404).json({
+        status: false,
+        message: "El rol no existe",
+      });
+      return;
+    }
+    let usr = {
+      ...req.body,
+    };
 
-			if (user.dataValues.email === usr.email) {
-				res.status(400).json({
-					status: false,
-					message: "El email ya existe",
-				});
-				return;
-			}
-		}
+    let user = await Users.findOne({
+      where: {
+        [Op.or]: [{ email: usr.email }, { username: usr.username }],
+      },
+    });
 
-		let createdUser = await Users.create(usr);
+    if (user) {
+      if (user.dataValues.username === usr.username) {
+        res.status(400).json({
+          status: false,
+          message: "El nombre de usuario ya existe",
+        });
+        return;
+      }
 
-		res.status(201).json({
-			status: true,
-			message: "Usuario creado exitosamente",
-			data: {
-				id: createdUser.dataValues.id,
-				email: createdUser.dataValues.email,
-				username: createdUser.dataValues.username,
-				nombre: createdUser.dataValues.nombre,
-				apellido: createdUser.dataValues.apellido,
-				phone: createdUser.dataValues.phone,
-				cedula: createdUser.dataValues.cedula,
-				role: { rol: role.rol, id: role.id },
-				roleId: createdUser.dataValues.roleId,
-			},
-		});
-	} catch (error) {
-		console.log(
-			colors.bgRed.white(
-				`Ocurrio un error en la creacion de usuarios. ${error}`
-			)
-		);
-		res.status(500).json({
-			status: false,
-			message: "Ocurrio un error en el servidor",
-		});
-	}
+      if (user.dataValues.email === usr.email) {
+        res.status(400).json({
+          status: false,
+          message: "El email ya existe",
+        });
+        return;
+      }
+    }
+
+    let createdUser = await Users.create(usr);
+
+    res.status(201).json({
+      status: true,
+      message: "Usuario creado exitosamente",
+      data: {
+        id: createdUser.dataValues.id,
+        email: createdUser.dataValues.email,
+        username: createdUser.dataValues.username,
+        nombre: createdUser.dataValues.nombre,
+        apellido: createdUser.dataValues.apellido,
+        phone: createdUser.dataValues.phone,
+        cedula: createdUser.dataValues.cedula,
+        role: { rol: role.rol, id: role.id },
+        roleId: createdUser.dataValues.roleId,
+      },
+    });
+  } catch (error) {
+    console.log(
+      colors.bgRed.white(
+        `Ocurrio un error en la creacion de usuarios. ${error}`
+      )
+    );
+    res.status(500).json({
+      status: false,
+      message: "Ocurrio un error en el servidor",
+    });
+  }
 };
 
 const putUser = async (req: ReqUpdateUser, res: ResponseAPI) => {
-	try {
-		let usr = req.body;
-		let { id } = req.params;
+  try {
+    let usr = req.body;
+    let { id } = req.params;
 
-		let user = await Users.findByPk(id);
+    let user = await Users.findByPk(id);
 
-		if (!user) {
-			res.status(400).json({
-				status: false,
-				message: "El usuario no existe",
-			});
-			return;
-		}
+    if (!user) {
+      res.status(400).json({
+        status: false,
+        message: "El usuario no existe",
+      });
+      return;
+    }
 
-		user = await user.update(usr);
-		res.status(200).json({
-			status: true,
-			message: "Datos actualizados correctamente",
-			data: user,
-		});
-		return;
-	} catch (error) {
-		console.log(
-			colors.bgRed.white(
-				`Ocurrio un error en la actualizacion de usuarios. ${error}`
-			)
-		);
-		res.status(500).json({
-			status: false,
-			message: "Ocurrio un error en el servidor",
-		});
-	}
+    user = await user.update(usr);
+    res.status(200).json({
+      status: true,
+      message: "Datos actualizados correctamente",
+      data: user,
+    });
+    return;
+  } catch (error) {
+    console.log(
+      colors.bgRed.white(
+        `Ocurrio un error en la actualizacion de usuarios. ${error}`
+      )
+    );
+    res.status(500).json({
+      status: false,
+      message: "Ocurrio un error en el servidor",
+    });
+  }
 };
 
 const getRoles = async (req: RequestAPI, res: ResponseAPI) => {
-	try {
-		let roles = await Roles.findAll({});
+  try {
+    let roles = await Roles.findAll({});
 
-		res.status(200).json({
-			status: true,
-			message: "Consulta exitosa",
-			data: roles,
-		});
-		return;
-	} catch (error) {
-		console.log(
-			colors.bgRed.white(`Ocurrio un error en la obtencion de roles. ${error}`)
-		);
-		res.status(500).json({
-			status: false,
-			message: "Ocurrio un error en el servidor",
-		});
-		return;
-	}
+    res.status(200).json({
+      status: true,
+      message: "Consulta exitosa",
+      data: roles,
+    });
+    return;
+  } catch (error) {
+    console.log(
+      colors.bgRed.white(`Ocurrio un error en la obtencion de roles. ${error}`)
+    );
+    res.status(500).json({
+      status: false,
+      message: "Ocurrio un error en el servidor",
+    });
+    return;
+  }
 };
 
 export { createUser, getOneUser, getUsers, putUser, getRoles };
---
+++ src/schemas/usersJoi.ts
@@ -0,0 +1,24 @@
+import Joi from "joi";
+import { join } from "path";
+import { abort } from "process";
+
+const schemaCreateUser = Joi.object({
+  username: Joi.string().alphanum().min(3).max(30).required(),
+
+  password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")),
+  nombre: Joi.string().min(4).max(50).required(),
+  apellido: Joi.string().min(4).max(50).required(),
+  phone: Joi.string().length(11),
+  cedula: Joi.string().pattern(new RegExp("^[0-9]*$")),
+  roleId: Joi.number().required(),
+
+  email: Joi.string().email({
+    minDomainSegments: 2,
+    tlds: { allow: ["com", "net"] },
+  }),
+});
+
+const validator = (schema) => (payload) =>
+  schema.validate(payload, { abortEarly: false });
+
+export const validateUser = validator(schemaCreateUser);
