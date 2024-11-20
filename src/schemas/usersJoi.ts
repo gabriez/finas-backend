39cdd7cae35ab3924bd6a2d02297a8d3720c7e6f
Gabriel <gabrieltorres2014@gmail.com>
2024-11-20 14:02:41 -0400

    test

--- src/schemas/usersJoi.ts
+++ src/schemas/usersJoi.ts
@@ -3,29 +3,44 @@ import { join } from "path";
 import { abort } from "process";
 
 const schemaCreateUser = Joi.object({
-  username: Joi.string().alphanum().min(3).max(30).required(),
+	username: Joi.string().alphanum().min(3).max(30).required(),
 
-  password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")),
-  nombre: Joi.string().min(4).max(50).required(),
-  apellido: Joi.string().min(4).max(50).required(),
-  phone: Joi.string().length(11),
-  cedula: Joi.string().pattern(new RegExp("^[0-9]*$")),
-  roleId: Joi.number().required(),
+	password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")),
+	nombre: Joi.string().min(4).max(50).required(),
+	apellido: Joi.string().min(4).max(50).required(),
+	phone: Joi.string().length(11),
+	cedula: Joi.string().pattern(new RegExp("^[0-9]*$")),
+	roleId: Joi.number().required(),
 
-  email: Joi.string().email({
-    minDomainSegments: 2,
-    tlds: { allow: ["com", "net"] },
-  }),
+	email: Joi.string().email({
+		minDomainSegments: 2,
+		tlds: { allow: ["com", "net"] },
+	}),
+});
+
+const schemaEditUser = Joi.object({
+	username: Joi.string().alphanum().min(3).max(30).required(),
+
+	// password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")),
+	nombre: Joi.string().min(4).max(50).required(),
+	apellido: Joi.string().min(4).max(50).required(),
+	phone: Joi.string().length(11),
+	cedula: Joi.string().pattern(new RegExp("^[0-9]*$")),
+	roleId: Joi.number().required(),
+
+	email: Joi.string().email({
+		minDomainSegments: 2,
+		tlds: { allow: ["com", "net"] },
+	}),
 });
 
 const schemaId = Joi.object({
-
-  id: Joi.number().required()
-  
+	id: Joi.number().required(),
 });
 
 const validator = (schema) => (payload) =>
-  schema.validate(payload, { abortEarly: false });
+	schema.validate(payload, { abortEarly: false });
 
 export const validateUser = validator(schemaCreateUser);
 export const validateId = validator(schemaId);
+export const validateEditUser = validator(schemaEditUser);
